46c5671febaa1d6088e6bd871a5e63669fdd1016
Skiboool <mateusz.skiba14@gmail.com>
2020-04-11 12:22:03 +0200

    quiz

---
+++ js/main.js
@@ -0,0 +1,173 @@
+const answers = document.body.querySelectorAll(".answer");
+let number = 0; // number of question
+let score = 0; // number of right answers
+let lineWidth = 100; // width of timer line
+let checkSelection = "true"; // check if the answer is selected
+
+const questions = [
+    {
+        ask: 'Jaka jest stolica Wenezueli?',
+        answers: [
+            {text: "Montevideo", correct: "false"},
+            {text: "Bogota", correct: "false"},
+            {text: "Caracas", correct: "true"},
+            {text: "Bangkok", correct: "false"}
+        ]
+    },
+    {
+        ask: 'Jakie państwo nie graniczy z Francją?',
+        answers: [
+            {text: "Belgia", correct: "false"},
+            {text: "Niemcy", correct: "false"},
+            {text: "Andora", correct: "false"},
+            {text: "Holandia", correct: "true"}
+        ]
+    },
+    {
+        ask: 'Jaka jest stolica Kanady?',
+        answers: [
+            {text: "Ottawa", correct: "true"},
+            {text: "Dublin", correct: "false"},
+            {text: "Juneau", correct: "false"},
+            {text: "Waszyngton", correct: "false"}
+        ]
+    },
+    {
+        ask: 'Jaka jest stolica Chin?',
+        answers: [
+            {text: "Pekin", correct: "true"},
+            {text: "Tokio", correct: "false"},
+            {text: "Seul", correct: "false"},
+            {text: "Hongkong", correct: "false"}
+        ]
+    },
+    {
+        ask: 'Jaka jest stolica Rumuni?',
+        answers: [
+            {text: "Budapeszt", correct: "false"},
+            {text: "Bukareszt", correct: "true"},
+            {text: "Warszawa", correct: "false"},
+            {text: "Lizbona", correct: "false"}
+        ]
+    }
+]
+
+const loadAnswer = () => {
+    document.querySelector("h1").textContent = questions[number].ask;
+    document.querySelector(".first").textContent = questions[number].answers[0].text;
+    document.querySelector(".first").dataset.correct = questions[number].answers[0].correct;
+
+    document.querySelector(".second").textContent = questions[number].answers[1].text;
+    document.querySelector(".second").dataset.correct = questions[number].answers[1].correct;
+
+    document.querySelector(".third").textContent = questions[number].answers[2].text;
+    document.querySelector(".third").dataset.correct = questions[number].answers[2].correct;
+
+    document.querySelector(".fourth").textContent = questions[number].answers[3].text;
+    document.querySelector(".fourth").dataset.correct = questions[number].answers[3].correct;
+}
+
+loadAnswer();
+
+// timer
+const countTime = () => {
+    if (lineWidth >= 0) {
+        lineWidth -= 0.1;
+        document.body.querySelector(".line").style.width = lineWidth + "%";
+    } else if (lineWidth <= 0) {
+        showQuestion();
+    }
+}
+
+timer = setInterval(countTime, 10);
+
+// after choice answer
+const showAnswer = (e) => {
+
+    clearInterval(timer);
+
+    if (checkSelection == "true") {
+        number += 1;
+    }
+
+    if (number == "5") {
+        document.body.querySelector(".nextBtn").textContent = "Zakończ"
+    }
+
+    if (checkSelection == "true") {
+        document.body.querySelector(".nextBtn").style.display = "block";
+        answers.forEach(answer => answer.style.borderBottom = "none");
+        answers.forEach(answer => answer.style.background = "red");
+    }
+
+    if (checkSelection == "true" && lineWidth >= 0) {
+        e.target.style.border = "4px solid black";
+        if (e.target.dataset.correct == "true") {
+            score += 1;
+        }
+    }
+
+    checkSelection = "false";
+
+    // show right answer
+    if (questions[number - 1].answers[0].correct == "true") {
+        document.querySelector(".first").style.background = "yellow";
+    } else if (questions[number - 1].answers[1].correct == "true") {
+        document.querySelector(".second").style.background = "yellow";
+    } else if (questions[number - 1].answers[2].correct == "true") {
+        document.querySelector(".third").style.background = "yellow";
+    } else if (questions[number - 1].answers[3].correct == "true") {
+        document.querySelector(".fourth").style.background = "yellow";
+    }
+
+}
+
+// after click next button
+const nextQuestion = () => {
+    checkSelection = "true";
+    lineWidth = 100;
+
+    timer = setInterval(countTime, 10);
+
+    document.body.querySelector(".nextBtn").style.display = "none";
+    answers.forEach(answer => answer.style.border = "none");
+    answers.forEach(answer => answer.style.borderBottom = "10px solid rgb(106, 106, 204)");
+    answers.forEach(answer => answer.style.background = "rgb(133, 133, 255)");
+
+    loadAnswer();
+}
+
+// show score after finish quiz
+const showScore = () => {
+    if (number == "5") {
+        document.body.querySelector(".scoreBox").style.display = "block";
+        document.body.querySelector(".score").style.display = "flex";
+        document.body.querySelector(".scoreTitle").innerHTML = `Gratulacje!</br>Twój wynik to ${score}/5`;
+
+        number = 0;
+
+        document.body.querySelector(".nextBtn").style.display = "none";
+        answers.forEach(answer => answer.style.border = "none");
+        answers.forEach(answer => answer.style.borderBottom = "10px solid rgb(106, 106, 204)");
+        answers.forEach(answer => answer.style.background = "rgb(133, 133, 255)");
+
+        clearInterval(timer);
+
+    }
+}
+
+// return quiz
+const restart = () => {
+    document.body.querySelector(".scoreBox").style.display = "none";
+    document.body.querySelector(".score").style.display = "none";
+    document.body.querySelector(".nextBtn").textContent = "Następne >"
+
+    timer = setInterval(countTime, 10);
+
+    loadAnswer();
+}
+
+answers.forEach(answer => answer.addEventListener("click", showAnswer))
+document.body.querySelector(".nextBtn").addEventListener("click", nextQuestion)
+document.body.querySelector(".nextBtn").addEventListener("click", showScore)
+document.body.querySelector(".return").addEventListener("click", restart)
